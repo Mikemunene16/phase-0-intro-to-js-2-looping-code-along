ea82bb59aae30791888997bf080188cf65ea9554
Mike <mike.munene@student.moringaschool.com>
2023-03-20 00:27:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const message = [];
+
+function writeCards(arr, event) {
+    for (let i = 0; i < arr.length; i++) {
+        message.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return message;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
